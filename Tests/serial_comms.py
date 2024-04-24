497d378da0614ba52da17100fdbfa35243ae02a9
Auto203 <slatu1@unh.newhaven.edu>
2024-04-24 12:56:15 -0400

    .

--- Tests/serial_comms.py
+++ Tests/serial_comms.py
@@ -1,7 +1,20 @@
-import serial
+import serial, time
 
-ser = serial.Serial('/dev/ttyUSB0')
-print(ser.name)
-ser.write('hello')
-line = ser.readline()
-ser.close() 
+#ser = serial.Serial('/dev/ttyUSB0')
+#with serial.Serial() as ser:
+ser = serial.Serial()    
+ser.baudrate = 9600
+ser.port = '/dev/ttyUSB0'
+#ser.open()
+    #ser.write(b'hello')
+#print(ser.name)
+while(True):
+	ser.open()
+	#ser = serial.Serial('/dev/ttyUSB0')
+	print("Sending..")
+	ser.write(b'hello')
+	#ser.close() 
+	#ser = serial.Serial('/dev/ttyUSB0')
+	#line = ser.readline()	
+	ser.close() 
+	time.sleep(2)
